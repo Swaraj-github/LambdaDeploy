e2dc94e7a9761b6bfcc98d7898cbbb76f6994950
Swaraj-github <52272171+Swaraj-github@users.noreply.github.com>
2020-08-10 11:40:50 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -3,5 +3,5 @@ import json
 
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = "Hello Swaraj"
+    response = "Hello Swaraj@TR"
     return {"statusCode": 200, "body": json.dumps(response)}
